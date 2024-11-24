dbdc7c586d71859120f6a877ed30fcda7ae8c55a
Tomas-Santoro <152992740+Tomas-Santoro@users.noreply.github.com>
2024-11-24 18:37:52 -0300

    Create interface Irenderer
    Se creo la interfaz render con trasform,draw alto y ancho y textura

---
+++ Game/renderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public interface Irenderer
+    {
+
+        Texture Texture { get; set; }
+
+
+        float Width { get; set; }
+        float Height { get; set; }
+
+
+        void Draw(TransformData transform);
+
+    }
+
+
+
+}
+
